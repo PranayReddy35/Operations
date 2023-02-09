1fdcccbf8cc59ecfe25cbaaaabc0beceaba8a609
PranayReddy35 <94630894+PranayReddy35@users.noreply.github.com>
2023-02-09 17:29:05 -0500

    Add files via upload

---
+++ operations-api/src/test/java/com/enterprise/operationsapi/OperationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.operationsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
